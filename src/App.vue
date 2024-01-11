4f445c0bf90c961a1dcb43b8235ba378236453c7
JermaineJunior <obietrice734@gmail.com>
2024-01-11 10:44:00 +0200

    Ui theme options updated

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,42 @@ import NavBar from './views/layout/NavBar.vue'
 
 <template>
   <main>
-   <NavBar />
+    <NavBar />
     <section class="grid items-center justify-center">
       <RouterView />
+    </section>
+    <section>
+      <!-- Bottom-Right Corner -->
+      <div class="fixed bottom-2 right-4 opacity-75">
+        <div class="dropdown dropdown-top">
+          <div tabindex="0" role="button" class="btn m-1">
+            Theme
+          </div>
+          <ul tabindex="0" class="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
+            <li><input type="radio" name="theme-dropdown"
+                class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Default" value="dark" />
+            </li>
+            <li><input type="radio" name="theme-dropdown"
+                class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Retro" value="retro" />
+            </li>
+            <li><input type="radio" name="theme-dropdown"
+                class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="garden"
+                value="garden" /></li>
+            <li><input type="radio" name="theme-dropdown"
+                class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Valentine"
+                value="valentine" /></li>
+            <li><input type="radio" name="theme-dropdown"
+                class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="cupcake"
+                value="cupcake" /></li>
+            <li><input type="radio" name="theme-dropdown"
+                class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="synthwave"
+                value="synthwave" /></li>
+            <li><input type="radio" name="theme-dropdown"
+                class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="luxury"
+                value="luxury" /></li>
+          </ul>
+        </div>
+      </div>
     </section>
   </main>
 </template>
--- src/views/layout/NavBar.vue
+++ src/views/layout/NavBar.vue
@@ -52,26 +52,6 @@ const handleLogOut = () => {
         </a>
       </div>
       <div class="navbar-end">
-        <label class="swap swap-rotate mr-2">
-
-          <!-- this hidden checkbox controls the state -->
-          <input type="checkbox" class="theme-controller" value="valentine" />
-
-          <!-- sun icon -->
-          <svg class="swap-on fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path
-              d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-          </svg>
-
-          <!-- moon icon -->
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="swap-off  w-8 h-8">
-            <path stroke-linecap="round" stroke-linejoin="round"
-              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-          </svg>
-
-
-        </label>
         <button class='btn btn-outline px-4 bg-base text-base-900' v-show="!isLoggedIn">
           <RouterLink class="my-2" to="/login">Sign In</RouterLink>
         </button>
